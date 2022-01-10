3d19a2535e877de930c509a190bd8bed7efc24c7
Neethi R Nair <neethi1090@gmail.com>
2022-01-09 23:52:41 -0500

    Home.vue modified

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,94 @@
 <template>
-  <hello-world />
+  <v-container class="home-container">
+    
+  </v-container>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
+  import { Component, Vue } from 'vue-property-decorator';
 
-  export default Vue.extend({
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+ 
+  
+  @Component({
+     components: {
+        
+      }
   })
+  export default class Home extends Vue {
+    private score: any = 0;
+    private sentence: any = ''; 
+    private renderComponent: any = true;
+    private maxScore: any = 10;
+    
+    get sentenceNumber(){
+      return this.$store.getters.getSentenceNumber;
+    }
+    
+    incrementScore(){
+      this.score = this.score+1;
+      this.$store.dispatch("setScore", this.score);
+      this.$store.dispatch("setSentenceNumber", this.sentenceNumber+1);
+      this.forceRerender();
+    } 
+    
+    forceRerender() {      
+        // Remove component from the DOM
+        this.renderComponent = false;
+
+        this.$nextTick(() => {
+          //Add the component back in
+          this.renderComponent = true;
+        });
+    }
+
+    get correctCount(){
+      return this.$store.getters.getCorrectCount;
+    }
+
+    get guessCount(){
+      return this.$store.getters.getGuessCount;
+    }
+
+    get winningScore(){
+      if(this.score === this.maxScore){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
+  }
+
 </script>
+<style >
+.home-container{
+  
+  height: 100vh;
+  border-radius: 8px;
+  margin-top: 50px;
+}
+.content{
+  background-color: #fff;
+  vertical-align: middle;
+  border-radius: 8px;
+}
+.win-container{
+  height: 80px;
+  border-radius: 8px;
+  margin-left: 20%;
+  margin-right: 20%;
+  font-size: 1.5rem;
+  
+}
+.disable-events {
+  pointer-events: none;
+}
+.win-outer{
+  height: 80vh;  
+   display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
